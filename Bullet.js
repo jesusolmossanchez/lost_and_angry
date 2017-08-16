279247a7019c4aeb38fcc2947567a7ab1209ad2c
jesusolmossanchez <jesusolmossanchez@gmail.com>
2017-08-16 10:04:34 +0200

    Posición correcta del disparo

--- Bullet.js
+++ Bullet.js
@@ -5,15 +5,17 @@ var Bullet = function(x, y, derecha, juego, jugador) {
     this.x    = x;
     this.y    = y + jugador.alto_/2;
     var negativo = 1;
+    var x_disparo = this.x + jugador.ancho_ + 35;
     if(!derecha){
         negativo = -1;
+        x_disparo = this.x - jugador.ancho_;
     }
     
-    this.x = this.x + negativo * jugador.ancho_ * 1.8;
+    this.x = x_disparo;
     
     this.size = 1;
-    this.xv   = negativo * 15;
-    this.yv   = (Math.random() - 0.5) * 4;
+    this.xv   = negativo * 35;
+    this.yv   = (Math.random() - 0.5) * 8;
 
 
 };
