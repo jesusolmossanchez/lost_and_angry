ac1e27b6b63f16a7dd85aae6ad0d34ff5a193298
jesusolmossanchez <jesusolmossanchez@gmail.com>
2017-08-30 15:50:38 +0200

    Zapatilla más recta y saliendo de más sitios

--- Zapatilla.js
+++ Zapatilla.js
@@ -3,24 +3,15 @@
 **************************************************/
 var Zapatilla = function(x, y, dx, dy, juego, boss) {
 
-
-
-    x_disparo = 
-
     this.x = x - boss.ancho_/4;
-    var alto_boss = Math.floor(boss.alto_/5);
-    this.y    = y + boss.alto_/2 + juego.randInt_(-alto_boss,alto_boss);
+    var alto_boss = Math.floor(boss.alto_/3);
     this.y    = y + boss.alto_/2 + juego.randInt_(-alto_boss,alto_boss);
 
-    
-    
-    
-    
     this.size = 1;
     var negativo = -1;
     this.angulo   = 90;
-    this.xv   = negativo * 15 + dx;
-    this.yv   = (Math.random() - 0.5) * 15 + dy;
+    this.xv   = (0.5 + Math.random()) * negativo * 5 + dx;
+    this.yv   = (Math.random() - 0.5) * 7 + dy;
 
 
 };
